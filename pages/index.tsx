89ac6c570cf62360967c37717e70092537a42f1c
Kate <katshe18@gmail.com>
2024-03-03 18:24:52 +1300

    Welcome message, added gradient to name, more styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import styles from "./index.module.scss";
 import { Navigation } from "../components/Navigation/Navigation";
+import Document from "./_document";
 
 export default function Page() {
   return (
-      <div className={styles.page}>
-        <Navigation />
-        <div className={styles.welcome}>
-          <h1>
-            Kate <br></br>Sheldon
-          </h1>
-          <p>frontend developer.</p>
-        </div>
+    <div className={styles.base}>
+      <Navigation />
+      <div className={styles.welcome}>
+          Hi, my name is <br/><span className={styles.name}>Kate.</span>
       </div>
+    </div>
   );
 }
