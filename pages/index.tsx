8fcae3c9b565f0b03674c70486ef8cf37721e733
Kate <katshe18@gmail.com>
2024-03-06 11:00:27 +1300

    Added 'About Me' text

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,13 @@ export default function Page() {
       <div className={styles.content}>
         <span className={styles.heading}>About me.</span>
         <div className={styles.text}>
-          I am a front end developer based in Auckland, NZ. I am an experienced
-          Search Engine Optimisation professional, looking to pivot my career
-          into frontend software development.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
         </div>
       </div>
     </div>
