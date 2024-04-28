a1662c68c7217aa2c70a308f31a40a66913af58b
Kate <katshe18@gmail.com>
2024-04-28 17:57:37 +1200

    Updated 'About' text

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,9 @@ export default function Page() {
       <div className={styles.content}>
         <span className={styles.heading}>About me.</span>
         <div className={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
+          I am a self-taught software developer with frontend work experience
+          and a background in Search Engine Optimisation. After a fulfilling
+          career break, I am now seeking a full-time role.
         </div>
       </div>
     </div>
